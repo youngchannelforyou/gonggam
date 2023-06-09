2e387c32b721fbaae88fd6223b7900fabb729117
Eomhyunjun <zzp1318@gmail.com>
2023-06-09 15:00:54 +0900

    [frontend] 가계부 페이지 - 라우트 생성 - 네비게이션생성

---
+++ frontend/src/pages/AccountBookPage/Community.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SideBar from '../../components/AcountBook/SideBar';
+
+function Community() {
+
+    return (
+        <div>
+            <SideBar />
+            <main></main>
+        </div>
+    );
+};
+
+export default Community;
+
